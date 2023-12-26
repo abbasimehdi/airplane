053664d4076887b3cabac318009408bd1a12f624
Abbasi Mehdi <abbasimhdi@gmail.com>
2023-12-26 13:22:50 +0330

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\Ticket\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::group(['prefix' => 'auth'], function($router) {
+    $router->post('/register', [\App\Http\Controllers\Api\V1\Auth\RegisterController::class, 'register']);
+    $router->post('/login', [\App\Http\Controllers\Api\V1\Auth\LoginController::class, 'login']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api'])->group(function ($router) {
+    $router->Apiresource('ticket', TicketController::class);
+    $router->get('user/{passportId}', [\App\Http\Controllers\Api\V1\User\UserController::class, 'show'])
+        ->middleware('checkPassportIdExist');
 });
